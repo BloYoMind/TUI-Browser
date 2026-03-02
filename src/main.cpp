141d5343f807604bb024abd4c66cf665bbb23935
BloYoMind <grady.jeremiah.smith@gmail.com>
2026-03-02 14:07:44 -0600

    Added user input

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,20 @@ size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) {
 }
 
 int main() {
+    std::string weblink;
+    std::cout << "What website do you want? (Include https:// at the begining)";
+    std::cin >> weblink;
+    
     curl_global_init(CURL_GLOBAL_DEFAULT);
     CURL* curl = curl_easy_init();
 
     if (curl) {
         std::string html;
-        curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
+        curl_easy_setopt(curl, CURLOPT_URL, weblink.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &html);
+        curl_easy_setopt(curl, CURLOPT_CAINFO, "C:/CURL/curl-8.18.0_5-win64-mingw/curl-8.18.0_5-win64-mingw/bin/curl-ca-bundle.crt");
+
 
         CURLcode res = curl_easy_perform(curl);
         if (res == CURLE_OK) {
